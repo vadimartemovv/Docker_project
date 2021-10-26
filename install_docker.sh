93790e571ccb5c015be455b20bcc4f9bd9b96dff
Anton Zubekhin <anton-zubehin@mail.ru>
2021-10-26 22:32:14 +0300

    Update install_docker.sh
    modified docker installation procedure

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
-sudo apt-get update
-sudo apt-get install docker-ce
-sudo apt-get install nvidia-docker
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
+
